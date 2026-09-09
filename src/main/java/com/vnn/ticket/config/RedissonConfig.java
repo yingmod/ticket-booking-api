c731e6e36be70e1bf87432e18f6d32dafe52bde1
VinhNg <vinhos51104@gmail.com>
2026-09-09 14:25:56 +0700

    feat: support cloud Redis password, SSL and CloudAMQP vhost

--- src/main/java/com/vnn/ticket/config/RedissonConfig.java
+++ src/main/java/com/vnn/ticket/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.vnn.ticket.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +17,19 @@ public class RedissonConfig {
     @Value("${spring.data.redis.port:6379}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean sslEnabled;
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
-        String address = String.format("redis://%s:%d", redisHost, redisPort);
+        String protocol = sslEnabled ? "rediss" : "redis";
+        String address = String.format("%s://%s:%d", protocol, redisHost, redisPort);
 
-        config.useSingleServer()
+        SingleServerConfig singleServerConfig = config.useSingleServer()
                 .setAddress(address)
                 .setTimeout(3000)
                 .setRetryAttempts(3)
@@ -29,6 +37,11 @@ public class RedissonConfig {
                 .setConnectionPoolSize(64)
                 .setConnectionMinimumIdleSize(10);
 
+        if (redisPassword != null && !redisPassword.isBlank()) {
+            singleServerConfig.setPassword(redisPassword);
+        }
+
         return Redisson.create(config);
     }
 }
+
